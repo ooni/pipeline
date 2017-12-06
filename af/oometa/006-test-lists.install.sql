b6a3bb65f2ffb134a3a973b7265e928ff0840f0b
Arturo Filastò <arturo@filasto.net>
2017-12-06 17:27:17 +0200

    Address comments by @darkk

--- af/oometa/006-test-lists.install.sql
+++ af/oometa/006-test-lists.install.sql
@@ -4,29 +4,32 @@ select _v.register_patch( '006-test-lists', ARRAY[ '005-repeated-report' ], NULL
 
 CREATE SEQUENCE cat_no_seq;
 
-CREATE TABLE url_categories
+CREATE TABLE url_category
 (
     cat_no INT NOT NULL default nextval('cat_no_seq') PRIMARY KEY,
     cat_code VARCHAR UNIQUE NOT NULL,
     cat_desc VARCHAR NOT NULL,
     cat_long_desc VARCHAR,
-    cat_old_codes VARCHAR
+    cat_old_codes VARCHAR[]
 );
+comment on table url_category is 'Contains the citizenlab URL category codes';
 
 CREATE SEQUENCE url_no_seq;
 
 CREATE TABLE url
 (
     url_no INT NOT NULL default nextval('url_no_seq') PRIMARY KEY,
-    url VARCHAR,
+    url VARCHAR NOT NULL,
     cat_no INT,
-    country_no INT,
-    date_added TIMESTAMP WITH TIME ZONE,
+    country_no INT NOT NULL,
+    date_added TIMESTAMP WITH TIME ZONE NOT NULL,
     source VARCHAR,
     notes VARCHAR,
-    active BOOLEAN,
+    active BOOLEAN NOT NULL,
     UNIQUE (url, country_no)
 );
+
+comment on table url is 'Contains information on URLs included in the citizenlab URL list';
 
 CREATE SEQUENCE country_no_seq;
 
@@ -35,8 +38,10 @@ CREATE TABLE country
     country_no INT NOT NULL default nextval('country_no_seq') PRIMARY KEY,
     full_name VARCHAR UNIQUE NOT NULL,
     name VARCHAR UNIQUE NOT NULL,
-    alpha_2 VARCHAR(2) UNIQUE NOT NULL,
-    alpha_3 VARCHAR(3) UNIQUE NOT NULL
+    alpha_2 CHAR(2) UNIQUE NOT NULL,
+    alpha_3 CHAR(3) UNIQUE NOT NULL
 );
+
+comment on table url is 'Contains country names and ISO codes';
 
 COMMIT;
