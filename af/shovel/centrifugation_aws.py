fd2c020c9280d288143bcf794756a9ddb45486ab
Arturo Filastò <arturo@filasto.net>
2019-03-29 00:13:55 +0100

    Set the exit code to distinguish non existent bucket from task failure

--- af/shovel/centrifugation_aws.py
+++ af/shovel/centrifugation_aws.py
@@ -1,7 +1,16 @@
 import os
+import sys
 import argparse
 
-from centrifugation import meta_pg
+import logging
+import autoclaving
+from centrifugation import meta_pg, open_s3_or_disk
+
+logging.getLogger('boto3').setLevel(logging.WARNING)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('nose').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+logging.getLogger('smart_open').setLevel(logging.WARNING)
 
 def parse_args():
     p = argparse.ArgumentParser(description='ooni-pipeline: public/autoclaved -> *')
@@ -14,7 +23,16 @@ def main():
     opt = parse_args()
     autoclaved_root = os.environ['AUTOCLAVED_ROOT']
     postgres = os.environ['POSTGRES_DSN']
-    meta_pg(autoclaved_root, opt.bucket, postgres)
+
+    try:
+        autoclaved_index = os.path.join(autoclaved_root, opt.bucket, autoclaving.INDEX_FNAME)
+        open_s3_or_disk(autoclaved_index)
+    except ValueError:
+        print("Did not find {}".format(autoclaved_index))
+        sys.exit(44)
+
+    print("Running: {} {}".format(autoclaved_root, opt.bucket))
+    #meta_pg(autoclaved_root, opt.bucket, postgres)
 
 if __name__ == '__main__':
     main()
