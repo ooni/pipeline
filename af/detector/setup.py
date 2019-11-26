6bf61899215cc81dcdb69162ae4e9704af78c088
Federico Ceratto <federico@openobservatory.org>
2019-11-26 19:45:54 +0000

    Update setup.py

--- af/detector/setup.py
+++ af/detector/setup.py
@@ -12,11 +12,9 @@ setup(
     name=NAME,
     python_requires=">=3.6.0",
     packages=["detector", "detector.tests"],
-    entry_points={"console_scripts": [
-        "detector=detector.detector:main",
-    ]},
+    entry_points={"console_scripts": ["detector=detector.detector:main",]},
     install_requires=REQUIRED,
     include_package_data=True,
     zip_safe=False,
-    package_data={'detector': ['views/*.tpl', 'static/*']},
+    package_data={"detector": ["views/*.tpl", "static/*", "data/country-list.json"]},
 )
