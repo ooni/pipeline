f472548924ff3884bef7994b01d303b7a39d04aa
Federico Ceratto <federico@openobservatory.org>
2021-06-10 22:09:33 +0100

    Implement draining

--- af/analysis/rotation.py
+++ af/analysis/rotation.py
@@ -12,7 +12,8 @@ CREATE UNLOGGED TABLE test_helper_instances (
     provider text NOT NULL,
     region text,
     ipaddr inet NOT NULL,
-    ipv6addr inet
+    ipv6addr inet,
+    draining_at timestamp without time zone
 );
 ALTER TABLE test_helper_instances OWNER TO shovel;
 
@@ -21,13 +22,24 @@ Example for /etc/ooni/rotation.conf
 [DEFAULT]
 token = CHANGEME
 db_uri = postgresql://shovel:CHANGEME@localhost/metadb
-live_droplets_count = 4
+active_droplets_count = 4
 size_slug = s-1vcpu-1gb
 image_name = debian-10-x64
 --
 
 Example for /etc/ooni/rotation_setup.sh
 --
+#!/bin/bash
+# Configure test-helper droplet
+# This script is run as root and with CWD=/
+set -euo pipefail
+exec 1>setup.log 2>&1
+echo "deb [trusted=yes] https://ooni-internal-deb.s3.eu-central-1.amazonaws.com unstable main" > /etc/apt/sources.list.d/ooni.list
+apt-key adv --verbose --keyserver hkp://keyserver.ubuntu.com --recv-keys 'B5A08F01796E7F521861B449372D1FF271F2DD50'
+apt-get update
+apt-get upgrade -qy
+echo > /etc/motd
+apt-get install -qy oohelperd
 --
 
 """
@@ -65,6 +77,17 @@ def add_droplet_to_db_table(db_conn, dr):
         db_conn.commit()
 
 
+def drain_droplet_in_db_table(db_conn, dr):
+    q = """UPDATE test_helper_instances
+    SET draining_at = NOW()
+    WHERE name = %s AND provider = 'Digital Ocean' AND region = %s
+    """
+    vals = [dr.name, dr.region["slug"]]
+    with db_conn.cursor() as cur:
+        cur.execute(q, vals)
+        db_conn.commit()
+
+
 def delete_droplet_from_db_table(db_conn, dr):
     q = """DELETE FROM test_helper_instances
     WHERE name = %s AND provider = 'Digital Ocean' AND region = %s
@@ -73,6 +96,30 @@ def delete_droplet_from_db_table(db_conn, dr):
     with db_conn.cursor() as cur:
         cur.execute(q, vals)
         db_conn.commit()
+
+
+@metrics.timer("destroy_drained_droplets")
+def destroy_drained_droplets(db_conn, api, draining_time_minutes, live_droplets):
+    q = """SELECT name FROM test_helper_instances
+    WHERE provider = 'Digital Ocean'
+    AND draining_at IS NOT NULL
+    AND draining_at < NOW() - interval '%s minutes ago'
+    ORDER BY draining_at
+    """
+    v = [draining_time_minutes]
+    with db_conn.cursor() as cur:
+        cur.execute(q, v)
+        oldest = cur.fetchone()
+
+    if oldest is None:
+        log.info("No droplet to destroy")
+        return
+
+    to_delete = [d for d in live_droplets if d.name == oldest]
+    for droplet in to_delete:
+        log.info(f"Destroying {droplet.name} droplet")
+        delete_droplet_from_db_table(db_conn, droplet)
+        droplet.destroy()
 
 
 @metrics.timer("spawn_new_droplet")
@@ -133,6 +180,24 @@ def load_conf():
     return cp["DEFAULT"]
 
 
+def drain_droplet_if_needed(db_conn, live_droplets, active_droplets_count):
+    q = """SELECT name FROM test_helper_instances
+        WHERE provider = 'Digital Ocean'
+        AND draining_at IS NULL
+    """
+    with db_conn.cursor() as cur:
+        cur.execute(q)
+        active = set(row[0] for row in cur.fetchall())
+    active_droplets = [d for d in live_droplets if d.name in active]
+    log.info(f"{len(active_droplets)} active droplets")
+
+    if len(active_droplets) > active_droplets_count:
+        by_age = sorted(active_droplets, key=lambda d: d.created_at)
+        oldest = by_age[0]
+        log.info(f"Draining {oldest.name} droplet")
+        drain_droplet_in_db_table(db_conn, oldest)
+
+
 @metrics.timer("run_time")
 def main():
     conf = load_conf()
@@ -140,9 +205,11 @@ def main():
     dig_oc_token = conf["token"]
     assert dig_oc_token
     assert len(dig_oc_token) == 64
+    draining_time_minutes = int(conf["draining_time_minutes"])
+    assert draining_time_minutes >= 0
 
-    live_droplets_count = int(conf["live_droplets_count"])
-    assert 0 <= live_droplets_count < 100
+    active_droplets_count = int(conf["active_droplets_count"])
+    assert 0 <= active_droplets_count < 100
 
     api = digitalocean.Manager(token=dig_oc_token)
     # Fetch all test-helper droplets
@@ -151,29 +218,26 @@ def main():
         assert TAG in d.tags
     live_droplets = [d for d in droplets if d.status == "active"]
     log.info(f"{len(droplets)} droplets")
-    log.info(f"{len(live_droplets)} active droplets")
+    log.info(f"{len(live_droplets)} powered on droplets")
 
     # Avoid failure modes where we destroy all VMs or create unlimited amounts
     # or churn too quickly
-    # TODO: age cehck
-    # TODO: deployer script
-    if len(live_droplets) <= live_droplets_count:
-        if len(droplets) > live_droplets_count + 2:
+    db_conn = psycopg2.connect(conf["db_uri"])
+
+    destroy_drained_droplets(db_conn, api, draining_time_minutes, live_droplets)
+
+    # Drain a droplet if needed
+    drain_droplet_if_needed(db_conn, live_droplets, active_droplets_count)
+
+    # Spawn a new droplet if needed
+    if len(live_droplets) <= active_droplets_count:
+        if len(droplets) > active_droplets_count + 2:
             log.error("Unexpected amount of running droplets")
             sys.exit(1)
         live_regions = set(d.region["slug"] for d in droplets)
-        db_conn = psycopg2.connect(conf["db_uri"])
         droplet = spawn_new_droplet(api, dig_oc_token, live_regions, conf)
         log.info(f"Droplet {droplet.name} ready at {droplet.ip_address}")
         add_droplet_to_db_table(db_conn, droplet)
-
-    if len(live_droplets) > live_droplets_count:
-        by_age = sorted(live_droplets, key=lambda d: d.created_at)
-        oldest = by_age[0]
-        log.info(f"Destroying {oldest.name} droplet")
-        db_conn = psycopg2.connect(conf["db_uri"])
-        oldest.destroy()
-        delete_droplet_from_db_table(db_conn, oldest)
 
 
 if __name__ == "__main__":
